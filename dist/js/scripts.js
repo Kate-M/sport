c8136f61788f56bba7c3deaf7d89e0bb9b268242
Kate-M <ekaterina.minich@gmail.com>
2018-04-10 13:32:27 +0300

    fix:fixed upcoming events section

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -13444,38 +13444,55 @@ if ( typeof define === 'function' && define.amd ) {
 
 })( window );
 
-$('.events-slider').slick({
-    infinite: true,
-    dots:true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    centerPadding: '40px',
-    customPaging : function(slider, i) {
-      var thumb = $(slider.$slides[i]).data();
-      return '<button class="slick-dots-item"></button>';
+$(document).ready(function () {
+  (function () {
+    $('.events-slider').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      centerPadding: '40px',
+      customPaging: function (slider, i) {
+        var thumb = $(slider.$slides[i]).data();
+        return '<button class="slick-dots-item"></button>';
       },
-    prevArrow: '<button class="prev-slick-button"></button>',
-    nextArrow: '<button class="next-slick-button"></button>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+      prevArrow: '<button class="prev-slick-button"></button>',
+      nextArrow: '<button class="next-slick-button"></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+          }
         }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          dots:false
-        }
-      }
-    ]
-  });
-      
+      ]
+    });
+  })();
+  (function () {
+    setMarginPrevButton();
+    function setMarginPrevButton() {
+      var dotsWidth = $('.slick-dots').width();
+      var containerButton = $('.slick-dots').parent();
+      var marginPrevButton = parseFloat(dotsWidth) + 30 + 'px';
+      containerButton.find('.prev-slick-button').css('margin-right', marginPrevButton);
+
+    };
+    $(window).resize(function () {
+      setMarginPrevButton();
+    });
+  })();
+});
+
 $('.partition-button').on('click', function () {
     $(this).next().toggleClass('open');
 });
