14e6e870d846065169a519ae78066ad75233ccc5
Kate-M <ekaterina.minich@gmail.com>
2018-04-16 17:05:30 +0300

    featur: added feature section

--- app/js/modules/featured-events-slider.js
+++ app/js/modules/featured-events-slider.js
@@ -1,36 +1,45 @@
-// $(document).ready(function () {
-//     (function () {
-//       $('.featured-events-slider').slick({
-//         infinite: true,
-//         dots: true,
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         customPaging: function (slider, i) {
-//           var thumb = $(slider.$slides[i]).data();
-//           return '<button class="slick-dots-item"></button>';
-//         },
-//         prevArrow: '<button class="event-arrow prev-button"></button>',
-//         nextArrow: '<button class="event-arrow next-button"></button>',
-//         responsive: [
-//           {
-//             breakpoint: 1024,
-//             settings: {
-//               slidesToShow: 4,
-//               slidesToScroll: 2
-//             }
-//           },
-//           {
-//             breakpoint: 768,
-//             settings: {
-//               slidesToShow: 2,
-//               slidesToScroll: 1,
-//               arrows: false,
-//               dots: false
-//             }
-//           }
-//         ]
-//       });
-//     })();
-//     setMarginPrevButton();
-//   });
+$(document).ready(function () {
+    (function () {
+      $('.featured-events-slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        customPaging: function (slider, i) {
+          var thumb = $(slider.$slides[i]).data();
+          return '<button class="slick-dots-item"></button>';
+        },
+        prevArrow: '<button class="event-arrow prev-button"></button>',
+        nextArrow: '<button class="event-arrow next-button"></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              arrows: false,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              dots: false
+            }
+          }
+        ]
+      });
+    })();
+    setMarginPrevButton();
+  });
   
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -13376,8 +13376,6 @@ return jQuery;
 }));
 
 
-
-
 ( function( window ) {
 
 'use strict';
@@ -13480,41 +13478,50 @@ $(document).ready(function () {
   };
 });
 
-// $(document).ready(function () {
-//     (function () {
-//       $('.featured-events-slider').slick({
-//         infinite: true,
-//         dots: true,
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         customPaging: function (slider, i) {
-//           var thumb = $(slider.$slides[i]).data();
-//           return '<button class="slick-dots-item"></button>';
-//         },
-//         prevArrow: '<button class="event-arrow prev-button"></button>',
-//         nextArrow: '<button class="event-arrow next-button"></button>',
-//         responsive: [
-//           {
-//             breakpoint: 1024,
-//             settings: {
-//               slidesToShow: 4,
-//               slidesToScroll: 2
-//             }
-//           },
-//           {
-//             breakpoint: 768,
-//             settings: {
-//               slidesToShow: 2,
-//               slidesToScroll: 1,
-//               arrows: false,
-//               dots: false
-//             }
-//           }
-//         ]
-//       });
-//     })();
-//     setMarginPrevButton();
-//   });
+$(document).ready(function () {
+    (function () {
+      $('.featured-events-slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        customPaging: function (slider, i) {
+          var thumb = $(slider.$slides[i]).data();
+          return '<button class="slick-dots-item"></button>';
+        },
+        prevArrow: '<button class="event-arrow prev-button"></button>',
+        nextArrow: '<button class="event-arrow next-button"></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              arrows: false,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              dots: false
+            }
+          }
+        ]
+      });
+    })();
+    setMarginPrevButton();
+  });
   
 $('.partition-button').on('click', function () {
     $(this).next().toggleClass('open');
@@ -13612,3 +13619,5 @@ $(document).ready(function () {
   })();
   setMarginPrevButton();
 });
+
+
