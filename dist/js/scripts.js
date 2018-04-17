479c0b53eb0e5e0e0df7af95ece2e482fbff52f7
Kate-M <ekaterina.minich@gmail.com>
2018-04-18 00:49:45 +0300

    fix:deleted tabs js code, fixed font-size in header

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -13592,24 +13592,7 @@ $('.navbar-top-button').on('click', function () {
       setMarginPrevButton();
     });
 
-$(document).ready(function () {
-    var labelsTabs = $('label.partition-name-tab').toArray();
-    var inputsTabs = $('.tab-input').toArray();
 
-    inputsTabs.forEach(function (el) {
-        el.addEventListener("change", changeActiveTab, false);
-    })
-
-    function changeActiveTab(e) {
-        labelsTabs.forEach(function (el) {
-            if (!el.control.checked) {
-                el.classList.remove('active');
-            }
-        })
-        var labelFor = e.srcElement.attributes["id"].value;
-        e.srcElement.labels[0].classList.add('active');
-    }
-});
 
 var endTime = new Date("Sep 4, 2018 05:01:50").getTime();
 var timer = setInterval(function () {
